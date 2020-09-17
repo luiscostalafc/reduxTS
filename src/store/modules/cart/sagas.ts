87de837cf34ba7567e81bfe3684a120c7f3e498d
luiscostalafc <luiscostalafc@gmail.com>
2020-09-17 08:15:44 -0300

    Dividindo as actions

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,12 +1,19 @@
-import { all, takeLatest } from 'redux-saga/effects'
-import { addProductToCart } from './actions'
+import { all, takeLatest, select } from 'redux-saga/effects'
+import { IState } from '../..'
+import { addProductToCartRequest } from './actions'
 
-type CheckProductStockRequest = ReturnType<typeof addProductToCart>
+type CheckProductStockRequest = ReturnType<typeof addProductToCartRequest>
 
 function* checkProductStock({ payload }: CheckProductStockRequest) {
-  
+  const { product } = payload
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0
+  })
+
+  console.log(currentQuantity)
 }
 
 export default all([
-  takeLatest('ADD_PRODUCT_TO_CART', checkProductStock)
+  takeLatest('ADD_PRODUCT_TO_CART_REQUEST', checkProductStock)
 ])
